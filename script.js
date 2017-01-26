9b252becab443c961c7936997ce67eb49617ba26
victorli5674 <victorli5674@yahoo.com>
2017-01-25 22:32:42 -0800

    Able to fetch images and status of specific stream

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+
     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2?callback=?', function(data) {
         console.log(data);
     });
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/FreeCodeCamp?callback=?', function(data) {
+        console.log(data);
+    });
+    
 });
