0e720ef3ac349a75c90248f50c2a4c4478c01064
victorli5674 <victorli5674@yahoo.com>
2017-01-26 22:27:23 -0800

    displayed with all the information a row should have

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 
+    // streamArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2?callback=?', function(data) {
-        console.log(data);
+        $('.container').append(
+            '<div class="row">' +
+                '<img src="' + data.stream.channel.logo + '" alt="" class="img-circle">' +
+                '<h4 class="stream_status">' +
+                    'Online | ' + data.stream.channel.name + ' | ' + data.stream.channel.game + ':' + data.stream.channel.status +
+                '</h4>' +
+            '</div>'
+        )
+        console.log(data.stream);
     });
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/FreeCodeCamp?callback=?', function(data) {
-        console.log(data);
-    });
-    
+
 });
