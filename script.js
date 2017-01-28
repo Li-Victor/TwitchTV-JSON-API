ff9eee65d26ca12bf5c15c6f82b50a28f89fdec0
victorli5674 <victorli5674@yahoo.com>
2017-01-28 02:14:40 -0800

    finished with finding the offline channel stream image and channel name

--- script.js
+++ script.js
@@ -13,14 +13,38 @@ $(document).ready(function() {
 
                         '<div class="col-sm-9">' +
                             '<h4 class="stream_status">' +
-                                'Online | ' + data.stream.channel.name + ' | ' + data.stream.channel.game + ':' + data.stream.channel.status +
+                                'Online | <a href="' + data.stream.channel.url + '" target="_blank">' + data.stream.channel.name + '</a> | ' + data.stream.channel.game + ':' + data.stream.channel.status +
                             '</h4>' +
                         '</div>' +
 
                     '</div>'
-                )
+                );
+            } else {
+
+                var channel = data._links.channel;
+                channel = channel.slice(channel.lastIndexOf('/'));
+
+                $.getJSON('https://wind-bow.gomix.me/twitch-api/channels' + channel + '?callback?', function(data) {
+                    $('.container').append(
+                        '<div class="row">' +
+                            '<div class="col-sm-3">' +
+                                '<img src="' + data.logo + '" alt="" class="img-circle">' +
+                            '</div>' +
+
+                            '<div class="col-sm-9">' +
+                                '<h4 class="stream_status">' +
+                                    'Offline | <a href="' + data.url + '" target="_blank">' + data.display_name + '</a>' +
+                                '</h4>' +
+                            '</div>' +
+
+                        '</div>'
+                    );
+                });
             }
+
         });
+
+
     }
 
 });
