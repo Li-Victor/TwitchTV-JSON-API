108b7ecde356fab0f7301a4a7e9d561a8d2258a3
victorli5674 <victorli5674@yahoo.com>
2017-01-27 23:33:12 -0800

    added some logic and some streams

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
 
-    // streamArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    streamArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2?callback=?', function(data) {
-        $('.container').append(
-            '<div class="row">' +
-                '<img src="' + data.stream.channel.logo + '" alt="" class="img-circle">' +
-                '<h4 class="stream_status">' +
-                    'Online | ' + data.stream.channel.name + ' | ' + data.stream.channel.game + ':' + data.stream.channel.status +
-                '</h4>' +
-            '</div>'
-        )
-        console.log(data.stream);
-    });
+    for (var stream in streamArray) {
+        $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + streamArray[stream] + '?callback?', function(data) {
+            if(data.stream) {
+                $('.container').append(
+                    '<div class="row">' +
+                        '<div class="col-sm-3">' +
+                            '<img src="' + data.stream.channel.logo + '" alt="" class="img-circle">' +
+                        '</div>' +
+
+                        '<div class="col-sm-9">' +
+                            '<h4 class="stream_status">' +
+                                'Online | ' + data.stream.channel.name + ' | ' + data.stream.channel.game + ':' + data.stream.channel.status +
+                            '</h4>' +
+                        '</div>' +
+
+                    '</div>'
+                )
+            }
+        });
+    }
 
 });
