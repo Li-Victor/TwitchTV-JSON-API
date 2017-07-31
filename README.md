ab7c94ad3d9bfd1dd3913a22d0de3371f85630c8
Victor Li <victorli5674@yahoo.com>
2017-07-30 22:53:01 -0700

    fixed styling and README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TwitchTV JSON API
-Using the TwitchTV JSON API to see which favorite people are currently streaming on Twitch.tv
+Using the [**TwitchTV API**](https://dev.twitch.tv/docs) to see which favorite people are currently streaming on Twitch.tv
 
-There is a status output and clicking on it redirects user to the stream. If a Twitch user is currently streaming, there will be 
-additional details about what they are streaming. There will be a placeholder notification if a streamer has closed their Twitch account or if the account does not exist. 
+There is a status output and clicking on it redirects user to the stream. If a Twitch user is currently streaming, there will be
+additional details about what they are streaming. There will be a placeholder notification if a streamer has closed their Twitch account or if the account does not exist.
 
 Made with HTML, CSS, Bootstrap, and JQuery
--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+'use strict';
+
 $(document).ready(function() {
 
-    streamArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    var streamArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 
     for (var stream in streamArray) {
-        $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + streamArray[stream] + '?callback?', function(data) {
+        var stream = 'https://wind-bow.glitch.me/twitch-api/streams/' + streamArray[stream] + '?callback?';
+
+        $.getJSON(stream, function(data) {
+
             if(data.stream) {
                 $('.container').append(
                     '<div class="row">' +
                         '<div class="col-sm-3">' +
-                            '<img src="' + data.stream.channel.logo + '" alt="" class="img-circle">' +
+                            '<img src="' + data.stream.channel.logo + '" alt="" class="img-thumbnail">' +
                         '</div>' +
 
                         '<div class="col-sm-9">' +
-                            '<h4 class="stream_status">' +
-                                'Online | <a href="' + data.stream.channel.url + '" target="_blank">' + data.stream.channel.name + '</a> | ' + data.stream.channel.game + ':' + data.stream.channel.status +
-                            '</h4>' +
+                            'Online | <a href="' + data.stream.channel.url + '" target="_blank">' + data.stream.channel.name + '</a> | ' + data.stream.channel.game + ':' + data.stream.channel.status +
                         '</div>' +
 
                     '</div>'
@@ -24,26 +27,47 @@ $(document).ready(function() {
                 var channel = data._links.channel;
                 channel = channel.slice(channel.lastIndexOf('/'));
 
-                $.getJSON('https://wind-bow.gomix.me/twitch-api/channels' + channel + '?callback?', function(data) {
-                    $('.container').append(
-                        '<div class="row">' +
-                            '<div class="col-sm-3">' +
-                                '<img src="' + data.logo + '" alt="" class="img-circle">' +
-                            '</div>' +
+                $.getJSON('https://wind-bow.glitch.me/twitch-api/channels' + channel + '?callback?', function(data) {
 
-                            '<div class="col-sm-9">' +
-                                '<h4 class="stream_status">' +
+                    //Placeholder user image
+                    if(!data.logo) data.logo = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
+
+                    if(data.error) {
+
+                        $('.container').append(
+                            '<div class="row">' +
+                                '<div class="col-sm-3">' +
+                                    '<img src="' + data.logo + '" alt="" class="img-thumbnail">' +
+                                '</div>' +
+
+                                '<div class="col-sm-9">' +
+                                    data.message +
+                                '</div>' +
+
+                            '</div>'
+                        );
+
+                    } else {
+
+                        $('.container').append(
+                            '<div class="row">' +
+                                '<div class="col-sm-3">' +
+                                    '<img src="' + data.logo + '" alt="" class="img-thumbnail">' +
+                                '</div>' +
+
+                                '<div class="col-sm-9">' +
                                     'Offline | <a href="' + data.url + '" target="_blank">' + data.display_name + '</a>' +
-                                '</h4>' +
-                            '</div>' +
+                                '</div>' +
 
-                        '</div>'
-                    );
+                            '</div>'
+                        );
+
+                    }
+
                 });
             }
 
         });
-
 
     }
 
